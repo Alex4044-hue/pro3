fd21685fe2ab6fa4ed0d5695702ea34fc5f851b5
alex <potfaleana@gmail.com>
2026-09-22 13:10:44 +0200

    fiqw

--- src/main/java/dk/via/pro3/springbootgrpcserverexample/service/VinylService.java
+++ src/main/java/dk/via/pro3/springbootgrpcserverexample/service/VinylService.java
@@ -1,9 +1,19 @@
 package dk.via.pro3.springbootgrpcserverexample.service;
 
+import dk.via.pro3.springbootgrpcserverexample.model.VinylRecord;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class VinylService
 {
+  private final Map<String, VinylRecord> vinylMap= new ConcurrentHashMap<>();
 
-}
+  public VinylRecord addVinyl(String name)
+  {
+
+    var id=
+    }
+  }
