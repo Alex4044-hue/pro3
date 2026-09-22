a6fdf6d5c7659e42958dac47a2d384bd72bbd3d7
alex <potfaleana@gmail.com>
2026-09-22 13:04:32 +0200

    fiqw

---
+++ src/main/java/dk/via/pro3/springbootgrpcserverexample/model/VinylRecord.java
@@ -0,0 +1,6 @@
+package dk.via.pro3.springbootgrpcserverexample.model;
+
+public record VinylRecord(String id, String name)
+{
+
+}
---
+++ src/main/java/dk/via/pro3/springbootgrpcserverexample/service/VinylService.java
@@ -0,0 +1,9 @@
+package dk.via.pro3.springbootgrpcserverexample.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class VinylService
+{
+
+}
